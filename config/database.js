65fe68bc11abececc0f38e4a802c71cdefdef14c
Titenq <titenq@gmail.com>
2019-12-28 15:32:50 -0300

    package

--- config/database.js
+++ config/database.js
@@ -18,7 +18,7 @@ module.exports = {
   | interacting with SQL databases.
   |
   */
-  connection: Env.get('DB_CONNECTION', 'sqlite'),
+  connection: Env.get('DB_CONNECTION', 'pg'),
 
   /*
   |--------------------------------------------------------------------------
@@ -86,8 +86,8 @@ module.exports = {
   pg: {
     client: 'pg',
     connection: {
-      host: Env.get('DB_HOST', DATABASE_URL.hostname),
-      port: Env.get('DB_PORT', DATABASE_URL.port),
+      host: Env.get('DB_HOST', DATABASE_URL.host),
+      port: Env.get('DB_PORT', ''),
       user: Env.get('DB_USER', DATABASE_URL.username),
       password: Env.get('DB_PASSWORD', DATABASE_URL.password),
       database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1))
