e46d0e773ce22fd4496af06d6217293f7fee432f
Titenq <titenq@gmail.com>
2019-12-28 15:37:20 -0300

    hostname

--- config/database.js
+++ config/database.js
@@ -86,7 +86,7 @@ module.exports = {
   pg: {
     client: 'pg',
     connection: {
-      host: Env.get('DB_HOST', DATABASE_URL.host),
+      host: Env.get('DB_HOST', DATABASE_URL.hostname),
       port: Env.get('DB_PORT', ''),
       user: Env.get('DB_USER', DATABASE_URL.username),
       password: Env.get('DB_PASSWORD', DATABASE_URL.password),
